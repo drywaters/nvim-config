afd8d4b659b15e48d842b50f5fbf863930b7e8af
Daniel Waters <daniel@siren.local>
2024-07-14 17:53:51 -0400

    Adds markdown LSP and linting

--- init.lua
+++ init.lua
@@ -587,6 +587,7 @@ require('lazy').setup({
             },
           },
         },
+        vale_ls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -905,7 +906,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.lazygit',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.neo-test',
 
