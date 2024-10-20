d58ab54bad0f8ade44111384acdb2bbd0474dc05
Daniel Waters <danwater1@gmail.com>
2024-10-20 13:09:53 -0400

    Config updates

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -95,7 +95,7 @@ return {
     }
     require('dap-vscode-js').setup {
       -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-      debugger_path = '/User/daniel/projects/vscode-js-debug', -- Path to vscode-js-debug installation.
+      debugger_path = '/User/daniel/.config/vscode-js-debug', -- Path to vscode-js-debug installation.
       -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
       adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
       -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
