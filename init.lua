ec5dfa11fb9b3e29220fa9fe734acb6f585fb143
Daniel Waters <danwater1@gmail.com>
2025-10-26 12:42:25 -0400

    Add custom plugins and formatters

--- init.lua
+++ init.lua
@@ -634,6 +634,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'gofmt', 'goimports' },
+
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
---
+++ lua/custom/plugins/notify.lua
@@ -0,0 +1,14 @@
+return {
+  'rcarriga/nvim-notify',
+  event = 'VeryLazy',
+  opts = {
+    stages = 'fade_in_slide_out',
+    timeout = 3000,
+    render = 'default',
+  },
+  config = function(_, opts)
+    local notify = require('notify')
+    notify.setup(opts)
+    vim.notify = notify
+  end,
+}
